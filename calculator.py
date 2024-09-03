d13965c119824eff3527646291f27321ef1567ff
Alexander Altukhov <sasha@littlebox.spb.ru>
2024-09-03 16:45:40 +0300

    modified:   calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,27 @@
 # Тестовое задание
 # Консольный калькулятор
 #
-lst = input().split()
-if len(lst) > 3:
-    raise ValueError("Too many values to unpack")
-if lst[1] in ("+", "-", "*", "/"):
-    sign = lst[1]
-else:
-    raise ValueError("Invalid operation")
-try:
-    a, b = int(lst[0]), int(lst[2])
-except ValueError:
-    raise ValueError("Invalid literal for int()")
-if 1 <= a <= 10 and 1 <= b <= 10:
-    match sign:
-        case "+":
-            print(a + b)
-        case "-":
-            print(a - b)
-        case "*":
-            print(a * b)
-        case "/":
-            print(a // b)
-else:
-    raise ValueError("Invalid a or b for value [1, 10]")
+def main(input: str):
+    lst = input.split()
+    if len(lst) > 3:
+        raise ValueError("Too many values to unpack")
+    if lst[1] in ("+", "-", "*", "/"):
+        sign = lst[1]
+    else:
+        raise ValueError("Invalid operation")
+    try:
+        a, b = int(lst[0]), int(lst[2])
+    except ValueError:
+        raise ValueError("Invalid literal for int()")
+    if 1 <= a <= 10 and 1 <= b <= 10:
+        match sign:
+            case "+":
+                return a + b
+            case "-":
+                return a - b
+            case "*":
+                return a * b
+            case "/":
+                return a // b
+    else:
+        raise ValueError("Invalid a or b for value [1, 10]")
